8937d489d43e3dca564bf7d50df50da12458d422
Athulya <athulyarajs95@gmail.com>
2023-07-15 11:00:23 +0530

    small padding added along  with container

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -3,7 +3,7 @@ import Header from "../components/Header";
 
 export default function Layout() {
   return (
-    <div className="container mx-auto">
+    <div className="container mx-auto px-10">
       <Header />
       <Outlet />
     </div>
